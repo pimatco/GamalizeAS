b3e7993fba81b24152a78f53737bab5d202b2388
pimatco <pimatco@gmail.com>
2017-10-14 10:43:59 -0300

    Initial commit

---
+++ app/src/main/java/com/aula/gama/myapplication/MainActivity.java
@@ -0,0 +1,82 @@
+package com.aula.gama.myapplication;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+import com.aula.gama.myapplication.Screen2Activity.Screen2;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final LinearLayout ll = new LinearLayout(this);
+        getActionBar();
+        ActionBar actionBar = getActionBar();
+        actionBar.setIcon(R.drawable.gamaiconmdpi);
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, Screen2.class);
+                startActivity(intent);*/
+
+                for(int i = 0; i < 20; i++) {
+                    CheckBox cb = new CheckBox(getApplicationContext());
+                    cb.setText("I'm dynamic!");
+                    ll.addView(cb);
+                }
+            }
+        });
+
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
