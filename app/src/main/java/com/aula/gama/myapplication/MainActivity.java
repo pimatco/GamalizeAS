495ae8c370cf58ecee9f305d7a84a814b5973bca
pimatco <pimatco@gmail.com>
2017-10-14 11:22:31 -0300

    tmj2

--- app/src/main/java/com/aula/gama/myapplication/MainActivity.java
+++ app/src/main/java/com/aula/gama/myapplication/MainActivity.java
@@ -11,11 +11,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TableRow;
 import android.widget.Toast;
 
 import com.aula.gama.myapplication.Screen2Activity.Screen2;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,10 +31,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* final LinearLayout ll = new LinearLayout(this);
-        getActionBar();
-        ActionBar actionBar = getActionBar();
-        actionBar.setIcon(R.drawable.gamaiconmdpi);*/
+       //final LinearLayout ll = new LinearLayout(this);
+       // getActionBar();
+       // ActionBar actionBar = getActionBar();
+       // actionBar.setIcon(R.drawable.gamaiconmdpi);
 
         Context context = getApplicationContext();
         CharSequence text = "opened";
@@ -36,15 +43,32 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
 
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final CheckBox cb = new CheckBox(this);
+        ScrollView scrl = new ScrollView(this);
+        final LinearLayout lil = new LinearLayout(this);
+        lil.setOrientation(LinearLayout.VERTICAL);
+        scrl.addView(lil);
+        final EditText EDIT_TEXT = new EditText(this);
+        EDIT_TEXT.setText("");
+        lil.addView(EDIT_TEXT);
+        Button btn = new Button(this);
+        btn.setText("Add");
+        lil.addView(btn);
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                Intent intent = new Intent(MainActivity.this, Screen2.class);
-                startActivity(intent);*/
+                //Intent intent = new Intent(MainActivity.this, Screen2.class);
+                //startActivity(intent);
+                for(int i = 0; i < 20; i++) {
+                    CheckBox cb = new CheckBox(getApplicationContext());
+                    cb.setText("I'm dynamic!");
+                    lil.addView(cb);
+                }
 
             }
         });
