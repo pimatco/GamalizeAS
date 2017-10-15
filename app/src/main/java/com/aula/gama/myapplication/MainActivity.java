bddbf04cdd37c9ba5a842c8ffe5c1ce27f56bd43
raquelotonif <raquelotonifernandes@gmail.com>
2017-10-14 21:08:11 -0300

    firebase

--- app/src/main/java/com/aula/gama/myapplication/MainActivity.java
+++ app/src/main/java/com/aula/gama/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,7 +21,14 @@ import android.widget.ScrollView;
 import android.widget.TableRow;
 import android.widget.Toast;
 
+
 import com.aula.gama.myapplication.Screen2Activity.Screen2;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
 
 import java.util.List;
 
@@ -30,6 +38,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        FirebaseDatabase fire = FirebaseDatabase.getInstance();
+        DatabaseReference ref = fire.getReference().child("assigments");
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Object dt=dataSnapshot.getValue();
+                String a = dt.toString();
+                Log.d("DATASNAP", a);
+                //String x =  dataSnapshot.getChildren();
+            }
+
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
        //final LinearLayout ll = new LinearLayout(this);
        // getActionBar();
@@ -44,17 +74,7 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        final CheckBox cb = new CheckBox(this);
-        ScrollView scrl = new ScrollView(this);
-        final LinearLayout lil = new LinearLayout(this);
-        lil.setOrientation(LinearLayout.VERTICAL);
-        scrl.addView(lil);
-        final EditText EDIT_TEXT = new EditText(this);
-        EDIT_TEXT.setText("");
-        lil.addView(EDIT_TEXT);
-        Button btn = new Button(this);
-        btn.setText("Add");
-        lil.addView(btn);
+
 
 
         fab.setOnClickListener(new View.OnClickListener() {
@@ -62,13 +82,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("gamalizers");
+                DatabaseReference dataRef = myRef.child("assigments");
+                final CheckBox cb = (CheckBox) findViewById(R.id.checkBox);
+                dataRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String v = dataSnapshot.getValue(String.class);
+                        String value = dataSnapshot.getValue(String.class);
+                        cb.setText(v);
+                        android.util.Log.d("CREATE", "Value is: " + value);
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        android.util.Log.w("", "Failed to read value.", error.toException());
+                    }
+                });
                 //Intent intent = new Intent(MainActivity.this, Screen2.class);
                 //startActivity(intent);
-                for(int i = 0; i < 20; i++) {
-                    CheckBox cb = new CheckBox(getApplicationContext());
-                    cb.setText("I'm dynamic!");
-                    lil.addView(cb);
-                }
+
 
             }
         });
